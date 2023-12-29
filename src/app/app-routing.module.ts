e46aba3400a2565d84c86792a403fde56aae3583
Marie-Geneste <marie.e.geneste@gmail.com>
2023-12-29 22:58:08 +0100

    Signup-Page css done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 const routes: Routes = [
   {
-    path: "", component : LoginPageComponent
+    path: "", pathMatch: "full", component : LoginPageComponent
+  },
+  {
+    path: "signup", component : SignupPageComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    SideBarComponent
+    SideBarComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
